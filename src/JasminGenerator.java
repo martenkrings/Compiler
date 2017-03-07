32fbd1e379f5b5f8d134ca90bd0e28dfbf968b89
Sander Groot Wesseldijk <heukertje@gmail.com>
2017-03-07 08:51:31 +0100

    jasming generator file

---
+++ src/JasminGenerator.java
@@ -0,0 +1,192 @@
+/**
+ * Created by Sander on 6-3-2017.
+ */
+public class JasminGenerator extends DomboBaseVisitor {
+
+
+
+    @Override
+    public Object visitProgram(DomboParser.ProgramContext ctx) {
+        return super.visitProgram(ctx);
+    }
+
+    @Override
+    public Object visitStatement(DomboParser.StatementContext ctx) {
+        return super.visitStatement(ctx);
+    }
+
+    @Override
+    public Object visitVarDeclaration(DomboParser.VarDeclarationContext ctx) {
+        return super.visitVarDeclaration(ctx);
+    }
+
+    @Override
+    public Object visitGenericVarDeclaration(DomboParser.GenericVarDeclarationContext ctx) {
+        return super.visitGenericVarDeclaration(ctx);
+    }
+
+    @Override
+    public Object visitScope(DomboParser.ScopeContext ctx) {
+        return super.visitScope(ctx);
+    }
+
+    @Override
+    public Object visitFunctionBlock(DomboParser.FunctionBlockContext ctx) {
+        return super.visitFunctionBlock(ctx);
+    }
+
+    @Override
+    public Object visitExpression(DomboParser.ExpressionContext ctx) {
+        return super.visitExpression(ctx);
+    }
+
+    @Override
+    public Object visitVariableAssign(DomboParser.VariableAssignContext ctx) {
+        return super.visitVariableAssign(ctx);
+    }
+
+    @Override
+    public Object visitAddOp(DomboParser.AddOpContext ctx) {
+        return super.visitAddOp(ctx);
+    }
+
+    @Override
+    public Object visitNegateOp(DomboParser.NegateOpContext ctx) {
+        return super.visitNegateOp(ctx);
+    }
+
+    @Override
+    public Object visitMulOp(DomboParser.MulOpContext ctx) {
+        return super.visitMulOp(ctx);
+    }
+
+    @Override
+    public Object visitIntVariable(DomboParser.IntVariableContext ctx) {
+        return super.visitIntVariable(ctx);
+    }
+
+    @Override
+    public Object visitIntValue(DomboParser.IntValueContext ctx) {
+        return super.visitIntValue(ctx);
+    }
+
+    @Override
+    public Object visitCalcComparator(DomboParser.CalcComparatorContext ctx) {
+        return super.visitCalcComparator(ctx);
+    }
+
+    @Override
+    public Object visitBoolValue(DomboParser.BoolValueContext ctx) {
+        return super.visitBoolValue(ctx);
+    }
+
+    @Override
+    public Object visitLogicComparator(DomboParser.LogicComparatorContext ctx) {
+        return super.visitLogicComparator(ctx);
+    }
+
+    @Override
+    public Object visitBoolVariable(DomboParser.BoolVariableContext ctx) {
+        return super.visitBoolVariable(ctx);
+    }
+
+    @Override
+    public Object visitNotOp(DomboParser.NotOpContext ctx) {
+        return super.visitNotOp(ctx);
+    }
+
+    @Override
+    public Object visitStringAddOp(DomboParser.StringAddOpContext ctx) {
+        return super.visitStringAddOp(ctx);
+    }
+
+    @Override
+    public Object visitStringWithExpression(DomboParser.StringWithExpressionContext ctx) {
+        return super.visitStringWithExpression(ctx);
+    }
+
+    @Override
+    public Object visitStringVariable(DomboParser.StringVariableContext ctx) {
+        return super.visitStringVariable(ctx);
+    }
+
+    @Override
+    public Object visitStringValue(DomboParser.StringValueContext ctx) {
+        return super.visitStringValue(ctx);
+    }
+
+    @Override
+    public Object visitStringReadStatement(DomboParser.StringReadStatementContext ctx) {
+        return super.visitStringReadStatement(ctx);
+    }
+
+    @Override
+    public Object visitIfSingleStatement(DomboParser.IfSingleStatementContext ctx) {
+        return super.visitIfSingleStatement(ctx);
+    }
+
+    @Override
+    public Object visitIfElseStatement(DomboParser.IfElseStatementContext ctx) {
+        return super.visitIfElseStatement(ctx);
+    }
+
+    @Override
+    public Object visitIfElseIfStatement(DomboParser.IfElseIfStatementContext ctx) {
+        return super.visitIfElseIfStatement(ctx);
+    }
+
+    @Override
+    public Object visitWhile(DomboParser.WhileContext ctx) {
+        return super.visitWhile(ctx);
+    }
+
+    @Override
+    public Object visitFor(DomboParser.ForContext ctx) {
+        return super.visitFor(ctx);
+    }
+
+    @Override
+    public Object visitFunctionDeclaration(DomboParser.FunctionDeclarationContext ctx) {
+        return super.visitFunctionDeclaration(ctx);
+    }
+
+    @Override
+    public Object visitFunctionTotal(DomboParser.FunctionTotalContext ctx) {
+        return super.visitFunctionTotal(ctx);
+    }
+
+    @Override
+    public Object visitFunctionPara(DomboParser.FunctionParaContext ctx) {
+        return super.visitFunctionPara(ctx);
+    }
+
+    @Override
+    public Object visitFunction(DomboParser.FunctionContext ctx) {
+        return super.visitFunction(ctx);
+    }
+
+    @Override
+    public Object visitGlobalDec(DomboParser.GlobalDecContext ctx) {
+        return super.visitGlobalDec(ctx);
+    }
+
+    @Override
+    public Object visitPrintCommand(DomboParser.PrintCommandContext ctx) {
+        return super.visitPrintCommand(ctx);
+    }
+
+    @Override
+    public Object visitReadCommand(DomboParser.ReadCommandContext ctx) {
+        return super.visitReadCommand(ctx);
+    }
+
+    @Override
+    public Object visitReturnCommand(DomboParser.ReturnCommandContext ctx) {
+        return super.visitReturnCommand(ctx);
+    }
+
+    @Override
+    public Object visitParameter(DomboParser.ParameterContext ctx) {
+        return super.visitParameter(ctx);
+    }
+}
